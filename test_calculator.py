a51c20f7a1db70a5e76f6fca89acedef12d7c57a
cmonsalve-flowmetrix <camilo.monsalve@flowmetrix.co>
2025-08-04 15:57:44 -0500

    Update lambda_function.py (#17)
    * Update lambda_function.py
    
    * Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,19 @@
 # test_calculator.py
 import unittest
-from calculator import add, subtract
+from lambda_function import add, lambda_handler
 
-class TestCalculator(unittest.TestCase):
+class TestAddition(unittest.TestCase):
+
     def test_add(self):
         self.assertEqual(add(3, 2), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
 
-    def test_subtract(self):
-        self.assertEqual(subtract(3, 2), 1)
+    def test_lambda_handler_add(self):
+        event = {"operation": "add", "a": 10, "b": 15}
+        response = lambda_handler(event, None)
+        self.assertEqual(response["statusCode"], 200)
+        self.assertIn("25", response["body"])
 
 if __name__ == '__main__':
     unittest.main()
